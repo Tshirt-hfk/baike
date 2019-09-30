3d83cbe24dae27c3e90f2e2f41d1f37d182957e1
dlutoyc <747241998@qq.com>
2019-09-30 16:07:21 +0800

    zc

--- config/index.js
+++ config/index.js
@@ -12,19 +12,19 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target:"http://0.0.0.0:8081",
+        target:"http://localhost:8081",
         pathRewrite:{
           '^/api':"/api"
         }
       },
       '/resource':{
-        target:"http://0.0.0.0:8081",
+        target:"http://localhost:8081",
         pathRewrite:{
           '^/resource':'/resource'
         }
       },
       '/data':{
-        target:"http://0.0.0.0:6000",
+        target:"http://localhost:6000",
         pathRewrite:{
           '^/data':''
         }
@@ -32,7 +32,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
