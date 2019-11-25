1ecc0042a5453876497ab5f746a5759d12615334
dlutoyc <747241998@qq.com>
2019-11-25 23:13:09 +0800

    专题

--- src/views/subject/subjectIntro.vue
+++ src/views/subject/subjectIntro.vue
@@ -8,16 +8,29 @@
         <subjectCategoryCard :category="category.name"></subjectCategoryCard>
       </div>
     </div>
+    <div class="subjectUnknown">
+      <div class="subjectUnknown-header">
+        <span class="subjectUnknown-category">开放分类</span>
+      </div>
+      <div>
+        <div class="subjectUnknownIntro" v-for="subject in subjects" :key="subject.id">
+          <subjectUnknown :id="subject.id" :name="subject.name" :imageUrl="subject.imageUrl" :intro="subject.intro"></subjectUnknown>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import subjectCategoryCard from "./subjectIntro/subjectCategoryCard";
+import subjectUnknown from "./subjectIntro/subjectUnknown"
 
 export default {
   name: "subjectIntro",
   components: {
-    subjectCategoryCard
+    subjectCategoryCard,
+    subjectUnknown
   },
   data() {
     return {
@@ -25,6 +38,20 @@ export default {
         {
           name: "计算机科学与技术"
         }
+      ],
+      subjects: [
+        {
+          id: 0,
+          name: "河流",
+          imageUrl: "http://101.200.34.92:8081/resource/image/260ca63909524fb593be134d31d2b3ee.png",
+          intro: "啊啊"
+        },
+        {
+          id: 1,
+          name: "海洋",
+          imageUrl: "http://101.200.34.92:8081/resource/image/260ca63909524fb593be134d31d2b3ee.png",
+          intro: "啊啊"
+        },
       ]
     };
   }
@@ -43,5 +70,31 @@ export default {
   margin-left: 15px;
   margin-top: 4px;
   font-size: 20px;
+}
+.subjectUnknown {
+  width: 1200px;
+}
+.subjectUnknown-header {
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: 24px;
+  border: 1px solid #bfbfbf;
+  height: 35px;
+}
+.subjectUnknown-category {
+  float: left;
+  margin-left: 20px;
+  margin-top: 3px;
+  font-size: 20px;
+  color: #409eff;
+}
+.subjectUnknown-header::before {
+  content: "";
+  display: inline-block;
+  width: 10px;
+  height: 37px;
+  background-color: #4d95dc;
+  position: absolute;
+  margin-top: -1px;
 }
 </style>
---
+++ src/views/subject/subjectIntro/subjectUnknown.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-card class="unknown-card" :body-style="{ padding: '10px' }">
+    <div slot="header" class="clearfix">
+      <span style="font-size: 18px">{{name}}</span>
+      <el-button style="float: right; padding: 3px 0" type="text">查看</el-button>
+    </div>
+    <div>
+      <div class="subjectEntry" v-for="entry in entrys" :key="entry.id">{{entry.name}}</div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "subjectUnknown",
+  props: {
+    id: Number,
+    imageUrl: String,
+    name: String,
+    intro: String
+  },
+  data() {
+    return {
+      entrys: [
+        {
+          id: 0,
+          name: "黄河",
+        },
+        {
+          id: 1,
+          name: "长江",
+        },
+        {
+          id: 2,
+          name: "爱河",
+        },
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.image {
+  float: left;
+}
+.intro {
+  width: 150px;
+  float: right;
+  word-wrap: break-word;
+}
+.text {
+  font-size: 16px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.unknown-card {
+  width: 370px;
+  height: 150px;
+  float: left;
+  margin-left: 20px;
+  margin-top: 10px;
+}
+.el-card__header {
+  padding: 12px 15px;
+}
+.subjectEntry{
+  float: left;
+  margin-left: 5px;
+}
+</style>
