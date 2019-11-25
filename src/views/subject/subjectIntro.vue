9f69608545f01dd7b0255196716d975d6ae673fb
Tshirt-hfk <fanglei212@outlook.com>
2019-11-25 15:08:04 +0800

    专题主页

---
+++ src/views/subject/subjectIntro.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="subject-category-header">
+      <span class="subject-category-header-text">全部专题</span>
+    </div>
+    <div>
+      <div v-for="category in categories" :key="category.name">
+        <subjectCategoryCard :category="category.name"></subjectCategoryCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import subjectCategoryCard from "./subjectIntro/subjectCategoryCard";
+
+export default {
+  name: "subjectIntro",
+  components: {
+    subjectCategoryCard
+  },
+  data() {
+    return {
+      categories: [
+        {
+          name: "计算机科学与技术"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.subject-category-header {
+  margin-top: 10px;
+  border: 1px solid #409eff;
+  border-left: 5px solid #409eff;
+  height: 36px;
+}
+.subject-category-header-text {
+  float: left;
+  margin-left: 15px;
+  margin-top: 4px;
+  font-size: 20px;
+}
+</style>
