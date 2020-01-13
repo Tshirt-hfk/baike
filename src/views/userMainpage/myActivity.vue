bb92a22850ca8357f8f7a58c60748e59b0d8ddb5
hieratic-seal <1048292005@qq.com>
2020-01-13 20:42:54 +0800

    zc

---
+++ src/views/userMainpage/myActivity.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="em-layout">
+    <div class="userActivity">个人动态</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.em-layout{
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+}
+</style>
